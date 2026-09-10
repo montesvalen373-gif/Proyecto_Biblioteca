e8bdb8c9d6b5310ba82ae9597fbc8736137e79f0
Angie Valentina Buitrago Montes <montesvalen373@gmail.com>
2026-09-10 18:02:47 -0500

    codigo de la clasa Bibliteca

---
+++ src/Biblioteca.java
@@ -0,0 +1,58 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Biblioteca {
+    public ArrayList<Libro> libros;
+    private String[] genero = {
+            "Novela",
+            "Ciencia",
+            "Historia",
+            "Infantil",
+            "Tecnico",
+            "Fantacia",
+            "Poesia"
+    };
+    private HashMap<String, ArrayList<Libro>> librosAutor;
+
+    public Biblioteca(){
+        libros = new ArrayList<>();
+        librosAutor = new HashMap<>();
+
+    }
+    public void agregarLibros(Libro libro){
+        libros.add(libro);
+
+        String autor = libro.getAutor();
+
+        if (!librosAutor.containsKey(autor)){
+            librosAutor.put(autor, new ArrayList<>());
+        }
+        librosAutor.get(autor).add(libro);
+
+    }
+    public ArrayList<Libro> filtrarAutor(String autor){
+        if (librosAutor.containsKey(autor)){
+            return librosAutor.get(autor);
+        }
+        return new ArrayList<>();
+    }
+    public void eliminarLibros(String codigo){
+        for (int i = 0; i< libros.size(); i++){
+
+            if (codigo.equals(libros.get(i).getCodigo())){
+
+                Libro libro = libros.get(i);
+
+                libros.remove(i);
+
+                String autor = libro.getAutor();
+
+                librosAutor.get(autor).remove(libro);
+
+                break;
+            }
+        }
+    }
+}
+
+
